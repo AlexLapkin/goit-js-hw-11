55f7b68d6168d59bc7031dd4c702cc5468950206
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2023-09-21 03:25:35 +0300

    update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const searchForm = document.querySelector('.search-form');
 const contGallery = document.querySelector('.gallery');
-const btnLoad = document.querySelector('.load-more');
 
 let page = 1;
 let per_page = 40;
@@ -19,76 +18,84 @@ let galleryLightBox = new SimpleLightbox('.gallery a');
 // Функция прослушивания при отправке формы
    function onSubmit (event) {
    event.preventDefault();
-   btnLoad.classList.add("is-hidden");
    contGallery.innerHTML = '';
    const { searchQuery } = event.currentTarget.elements;
    searchImage(searchQuery.value, page)
    .then(function (resp) {
     
      const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = resp.data;
-      const { totalHits } = resp.data;
+     const { totalHits } = resp.data;
             
       if ((resp.data.hits).length > 0) {
       renderList(resp.data.hits, contGallery);
       
+       // SimpleLightbox
       galleryLightBox.refresh();
 
       Notiflix.Notify.success(`Hooray! We found ${totalHits} images`);
-      
-      btnLoad.classList.remove("is-hidden");
-      btnLoad.textContent = "Load more" ;
       softScroll();
-      }
+  }
     else {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.', {
           position: 'center-center',
           timeout: 3000,
-          } 
-        )
+          })
     }
    })
-    .catch (console.error (error));
-   }
-
-
-// Функция прослушивания кнопки Load more
- function onClickLoadMore () {
-  galleryLightBox.refresh();
-  page += 1;
-  let totalImages = per_page * page;
-   
-  const { searchQuery } = searchForm.elements;
-  
-   searchImage(searchQuery.value, page)
-  .then(function (resp) {
-   const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = resp.data;
-   const { totalHits } = resp.data;
-          
-     if ( totalImages > totalHits ) {
-       Notiflix.Notify.info(`We are sorry, but you have reached the end of search results`);
-       btnLoad.classList.add("is-hidden");
-      }
-     
-     renderList(resp.data.hits, contGallery);
-     softScroll();
-     galleryLightBox.refresh();
-    }
-   )
-   .catch(console.error (error));
+    .catch(error => console.error(error))
+    //.finally(searchForm.reset());
 }
 
 
-searchForm.addEventListener("submit", onSubmit);
-btnLoad.addEventListener ("click", onClickLoadMore);
+// Функция прослушивания кнопки Load more
+function onLoadMore() {
+  
+  page += 1;
+  let totalImages = per_page * page;
+  //galleryLightBox.destroy();
+    
+  const { searchQuery } = searchForm.elements;
+  
+  searchImage(searchQuery.value, page)
+  .then(function (resp) {
+   const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = resp.data;
+   const { totalHits } = resp.data;
+         
+   // SimpleLightbox
+   galleryLightBox.refresh();
+
+   if ( totalImages >= totalHits ) {
+       Notiflix.Notify.info(`We are sorry, but you have reached the end of search results`);
+       searchForm.reset();
+    }
+    else {
+      renderList(resp.data.hits, contGallery);
+      softScroll();
+    }
+    }
+   )
+   .catch(error => console.error(error)) ; 
+  }
 
 
 // Функция плавного скролла  
 function softScroll() {
-const { height: cardHeight } = contGallery.firstElementChild.getBoundingClientRect();
+  const { height: cardHeight } = contGallery.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+  top: cardHeight * 1,
+  behavior: "smooth",
+  });
+  }
+  
 
-window.scrollBy({
-top: cardHeight * 2,
-behavior: "smooth",
-});
-}
+// Функция определения низа страницы
+function endOfPage() {
+   if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+        onLoadMore();
+      }
+  }
+  
+// Прослушиватели событий
+searchForm.addEventListener("submit", onSubmit);
+window.addEventListener("scroll", endOfPage);
